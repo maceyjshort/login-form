6970bb4e9e5b7efab1496d93cbeb965f338bc631
maceyjshort <macey.walker1991@gmail.com>
2022-10-24 11:25:56 -0500

    Fix form testing

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,14 +1,19 @@
 import PropTypes from "prop-types";
 
-export default function Input({ label }) {
+export default function Input({ label, type }) {
   return (
     <div className="mt-4 flex flex-col">
       <label htmlFor={label}>{label}</label>
-      <input type={label} id={label} placeholder={label} />
+      <input type={type} id={label} placeholder={label} />
     </div>
   );
 }
 
 Input.propTypes = {
   label: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["email", "password"]),
+};
+
+Input.defaultProps = {
+  type: "text",
 };
--- src/components/Form/Input.test.jsx
+++ src/components/Form/Input.test.jsx
@@ -2,12 +2,21 @@ import { render, screen } from "@testing-library/react";
 import Input from "./Input";
 
 describe("Input", () => {
-  it("renders a submit button with the correct label, placeholder, an id", () => {
+  it("renders with the correct label, placeholder, and id and is of type text", () => {
     const myLabel = "label";
-    render(<Input Label={myLabel} />);
+    render(<Input label={myLabel} />);
 
     const input = screen.getByLabelText(myLabel);
 
     expect(input).toBeInTheDocument();
+    expect(input).toHaveAttribute("type", "text");
+  });
+
+  it("renders with the correct type", () => {
+    render(<Input label="label" type="email" />);
+
+    const input = screen.getByLabelText("label");
+
+    expect(input).toHaveAttribute("type", "email");
   });
 });
