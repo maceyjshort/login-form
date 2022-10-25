f6a2cc5f73fefed2ba166c3b5ce47e74611b4612
maceyjshort <macey.walker1991@gmail.com>
2022-10-25 10:23:24 -0500

    Add Commments about difference

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -17,3 +17,8 @@ Input.propTypes = {
 Input.defaultProps = {
   type: "text",
 };
+
+// Multi line code isn't working because why the flip not? DOn't want to waste anymore time trying to figure it out
+// anyway I will mainly talk about the uncontrolled on this document.
+// As I stated in the other branch, react docs claim that it is best to controlled versus uncontrolled
+// when using uncontrolled it takes less code, but there is also less controll on them (hence the name) with the less code, there is different syntax used, different code needed to communicate with the DOM
